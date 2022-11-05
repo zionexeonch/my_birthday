ec06fab8ce2720698df827166a62ef19e7e0fd4e
zionexeonch <zionexeon@gmail.com>
2022-11-05 19:16:03 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["index.html"],
+    theme: {
+        extend: {
+            fontFamily: {
+                manrope: ["Manrope"],
+                notoserifjp: ["Noto+Serif+JP"],
+                poppins: ["Poppins"],
+            },
+        },
+    },
+    plugins: [],
+}
